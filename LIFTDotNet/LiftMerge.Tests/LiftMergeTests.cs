b0748e86477f0516100f8b6581616ffd639399d2
hattonjohn <hattonjohn@44f4c372-9d29-0410-bcad-4118a2cb9d5c>
2008-02-13 09:02:58 +0000

    Various things to make bad updates less bad.

---
+++ LIFTDotNet/LiftMerge.Tests/LiftMergeTests.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace LiftMerge.Tests
+{
+    [TestFixture]
+    public class LiftMergeTests
+    {
+
+        [Test]
+        public void Test()
+        {
+            //TODO: run the LiftMerge command line app
+        }
+    }
+}
---
+++ LIFTDotNet/LiftMerge/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using LiftIO;
+
+namespace LiftMerge
+{
+    /// <summary>
+    /// ;ext.lift = external C:\WeSay\lib\LIFT\LIFTDotNet\output\debug\liftmerge  $base $local $other $output
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           string basePath = args[0];
+            string localPath = args[1];
+            string otherPath = args[2];
+            string outputPath = args[3];
+
+//            foreach (string s in args)
+//            {
+//                Console.WriteLine(s);
+//            }
+
+           Console.WriteLine("LiftMerge...");
+           using (StreamWriter output = File.CreateText(outputPath))
+            {
+                LiftIO.LiftVersionControlMerger merger =
+                    new LiftVersionControlMerger(File.ReadAllText(localPath), File.ReadAllText(otherPath), File.ReadAllText(basePath));
+               
+                //output.WriteLine("<!-- Processed by LiftMerge {0}, {1}, {2} -->", basePath, localPath, otherPath);
+                output.Write(merger.GetMergedLift());
+            }
+        }
+    }
+}
