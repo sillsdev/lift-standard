d5614c3909247e55a5312a9ac36c4d36346b3a5a
hattonjohn <hattonjohn@44f4c372-9d29-0410-bcad-4118a2cb9d5c>
2008-04-26 11:22:08 +0000

    Just put the two merge strategies in the correct files.

--- LIFTDotNet/LiftIO/Merging/DropTheirsMergeStrategy.cs
+++ LIFTDotNet/LiftIO/Merging/DropTheirsMergeStrategy.cs
@@ -4,23 +4,15 @@ using System.Xml;
 namespace LiftIO.Merging
 {
     /// <summary>
-    /// This strategy doesn't even try to put the entries together.  It just takes "their" entry
-    /// and sticks it in a merge failure field
+    /// This strategy doesn't even try to put the entries together.  It just returns ours.
     /// </summary>
-    public class PoorMansMergeStrategy : IMergeStrategy
+    public class DropTheirsMergeStrategy : IMergeStrategy
     {
         public string MakeMergedEntry(XmlNode ourEntry, XmlNode theirEntry, XmlNode unusedCommonEntry)
         {
-            XmlNode mergeNoteFieldNode = ourEntry.OwnerDocument.CreateElement("field");
-            LiftVersionControlMerger.AddAttribute(mergeNoteFieldNode, "type", "mergeConflict");
-            LiftVersionControlMerger.AddDateCreatedAttribute(mergeNoteFieldNode);
-            StringBuilder b = new StringBuilder();
-            b.Append("<trait name='looserData'>");
-            b.AppendFormat("<![CDATA[{0}]]>", theirEntry.OuterXml);
-            b.Append("</trait>");
-            mergeNoteFieldNode.InnerXml = b.ToString();
-            ourEntry.AppendChild(mergeNoteFieldNode);
             return ourEntry.OuterXml;
         }
+
+
     }
 }
--- LIFTDotNet/LiftIO/Merging/PoorMansMergeStrategy.cs
+++ LIFTDotNet/LiftIO/Merging/PoorMansMergeStrategy.cs
@@ -4,12 +4,22 @@ using System.Xml;
 namespace LiftIO.Merging
 {
     /// <summary>
-    /// This strategy doesn't even try to put the entries together.  It just returns ours.
+    /// This strategy doesn't even try to put the entries together.  It just takes "their" entry
+    /// and sticks it in a merge failure field
     /// </summary>
-    public class DropTheirsMergeStrategy : IMergeStrategy
+    public class PoorMansMergeStrategy : IMergeStrategy
     {
         public string MakeMergedEntry(XmlNode ourEntry, XmlNode theirEntry, XmlNode unusedCommonEntry)
         {
+            XmlNode mergeNoteFieldNode = ourEntry.OwnerDocument.CreateElement("field");
+            LiftVersionControlMerger.AddAttribute(mergeNoteFieldNode, "type", "mergeConflict");
+            LiftVersionControlMerger.AddDateCreatedAttribute(mergeNoteFieldNode);
+            StringBuilder b = new StringBuilder();
+            b.Append("<trait name='looserData'>");
+            b.AppendFormat("<![CDATA[{0}]]>", theirEntry.OuterXml);
+            b.Append("</trait>");
+            mergeNoteFieldNode.InnerXml = b.ToString();
+            ourEntry.AppendChild(mergeNoteFieldNode);
             return ourEntry.OuterXml;
         }
     }
