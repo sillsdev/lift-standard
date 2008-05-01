8a89bb8cedab7e7be4ced155ece7e1cdc5d98af0
hattonjohn <hattonjohn@44f4c372-9d29-0410-bcad-4118a2cb9d5c>
2008-05-01 02:33:25 +0000

    Added code from XmlUnit for diffing xml nodes. This will help with the merger.

--- trunk/LIFTDotNet/LiftIO.Tests/Merging/MultiTextMergerTests.cs
+++ trunk/LIFTDotNet/LiftIO.Tests/Merging/MultiTextMergerTests.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Xml;
 using LiftIO.Merging;
 using NUnit.Framework;
+using XmlUnit;
 
 namespace LiftIO.Tests.Merging
 {
---
+++ trunk/LIFTDotNet/LiftIO/Merging/xmldiff/DiffResult.cs
@@ -0,0 +1,64 @@
+/* From http://xmlunit.sourceforge.net/ Moved here because the original library is for testing, and 
+ * it is tied to nunit, which we don't want to ship in production
+ */
+
+namespace LiftIO.Merging.XmlDiff
+{
+	using System; 
+	using System.Text; 
+	
+    public class DiffResult {
+        private bool _identical = true;
+        private bool _equal = true;
+        private Difference _difference;
+    	private StringBuilder _stringBuilder;
+    	
+    	public DiffResult() {
+    		_stringBuilder = new StringBuilder();
+    	}
+        
+        public bool Identical {
+            get {
+                return _identical;
+            }
+        }
+        
+        public bool Equal {
+            get {
+                return _equal;
+            }
+        }
+        
+        public Difference Difference {
+            get {
+                return _difference;
+            }
+        }
+     
+        public string StringValue {
+        	get {
+	        	if (_stringBuilder.Length == 0) {
+	        		if (Identical) {
+	        			_stringBuilder.Append("Identical");        			
+	        		} else {
+	        			_stringBuilder.Append("Equal");
+	        		}
+	        	}
+	        	return _stringBuilder.ToString();
+        	}
+        }
+        
+        public void DifferenceFound(XmlDiff inDiff, Difference difference) {
+            _identical = false;
+            if (difference.MajorDifference) {
+                _equal = false;
+            }       
+            _difference = difference;
+        	if (_stringBuilder.Length == 0) {
+        		_stringBuilder.Append(inDiff.OptionalDescription);
+        	}
+        	_stringBuilder.Append(Environment.NewLine).Append(difference);
+        }        
+    }
+	
+}
